86de2d559ed36eca95424a7172bbe514c2092454
Guilherme Miyata <miyatagui120600@gmail.com>
2025-02-28 21:50:18 -0300

    private folder

---
+++ Tutorial 1/routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,8 @@
+export default function PrivateFoulder() {
+  return (
+    <div>
+      <h1>Private Folder</h1>
+      <p>This is a private foulder. It doesnt create a route</p>
+    </div>
+  );
+}
