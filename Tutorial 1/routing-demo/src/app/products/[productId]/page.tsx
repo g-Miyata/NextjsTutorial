75bd44076f8fe4d2aa09c5f312d865f3f1d7ab59
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-05 13:56:02 -0300

    dynamic metadata

--- Tutorial 1/routing-demo/src/app/products/[productId]/page.tsx
+++ Tutorial 1/routing-demo/src/app/products/[productId]/page.tsx
@@ -1,5 +1,18 @@
-const ProductDetails = async ({ params }: { params: { productId: string } }) => {
-  const productId = params.productId;
+import { Metadata } from 'next';
+
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  return {
+    title: `Product ${id}`,
+  };
+};
+
+const ProductDetails = async ({ params }: Props) => {
+  const productId = (await params).productId;
   return <h1>Details of Product {productId}</h1>;
 };
 
