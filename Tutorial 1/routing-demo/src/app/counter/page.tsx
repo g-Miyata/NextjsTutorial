dd9410d39484366a0aac462457129e126a04b74d
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-05 13:56:31 -0300

    handling metadata in client components

---
+++ Tutorial 1/routing-demo/src/app/counter/page.tsx
@@ -0,0 +1,12 @@
+import Counter from './counter';
+
+export const metadata = {
+  title: 'Counter',
+  description: 'A simple counter example',
+};
+
+const CounterPage = () => {
+  return <Counter />;
+};
+
+export default CounterPage;
