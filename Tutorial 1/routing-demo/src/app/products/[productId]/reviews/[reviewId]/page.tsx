c0f3a7e20bdb69c87270fdc6bf12a0ef3a02dcbd
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-05 16:33:47 -0300

    navigating programmatically: redirect

--- Tutorial 1/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ Tutorial 1/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -8,11 +8,12 @@
 // };
 
 // export default ProductReview;
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 export default async function ProductReview({ params }: { params: Promise<{ productId: string; reviewId: string }> }) {
   const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
-    notFound();
+    // notFound();
+    redirect('/products');
   }
   return (
     <h1>
