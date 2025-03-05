0f569dca4ac1ea7ea2d0fd9c68b06a8efed94f9a
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-05 17:43:25 -0300

    error handling

--- Tutorial 1/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ Tutorial 1/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -9,10 +9,18 @@
 
 // export default ProductReview;
 import { notFound, redirect } from 'next/navigation';
+
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
 export default async function ProductReview({ params }: { params: Promise<{ productId: string; reviewId: string }> }) {
   const { productId, reviewId } = await params;
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error('Error loading review');
+  }
   if (parseInt(reviewId) > 1000) {
-    // notFound();
+    notFound();
     redirect('/products');
   }
   return (
