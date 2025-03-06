b0339c1f672a3f5231515cdaf10fc155b19ae4a9
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-05 21:08:43 -0300

    server-side Recovering from Errors

--- Tutorial 1/routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ Tutorial 1/routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,20 @@
 'use client';
-
-export default function ErrorBoundary({ error }: { error: Error }) {
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void }) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
   return (
     <div>
-      <h1>{error.message}</h1>
+      <p>{error.message}</p>
+      <button onClick={() => reload()}>Try Again</button>
     </div>
   );
 }
+
+
