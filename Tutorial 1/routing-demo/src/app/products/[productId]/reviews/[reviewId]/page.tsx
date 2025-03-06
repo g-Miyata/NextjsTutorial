f5cba748e90bc90deeaf8ab548edaada732a7589
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-05 21:28:46 -0300

    Handling Errors in Layouts

--- Tutorial 1/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ Tutorial 1/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -10,15 +10,15 @@
 // export default ProductReview;
 import { notFound, redirect } from 'next/navigation';
 
-function getRandomInt(count: number) {
-  return Math.floor(Math.random() * count);
-}
+// function getRandomInt(count: number) {
+//   return Math.floor(Math.random() * count);
+// }
 export default async function ProductReview({ params }: { params: Promise<{ productId: string; reviewId: string }> }) {
   const { productId, reviewId } = await params;
-  const random = getRandomInt(2);
-  if (random === 1) {
-    throw new Error('Error loading review');
-  }
+  // const random = getRandomInt(2);
+  // if (random === 1) {
+  //   throw new Error('Error loading review');
+  // }
   if (parseInt(reviewId) > 1000) {
     notFound();
     redirect('/products');
