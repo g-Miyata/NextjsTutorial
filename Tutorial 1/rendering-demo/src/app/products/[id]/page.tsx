ea2be50d49e836e6b31085299d7ba428b2c4f824
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-07 14:22:02 -0300

    generateStaticParams

---
+++ Tutorial 1/rendering-demo/src/app/products/[id]/page.tsx
@@ -0,0 +1,18 @@
+export async function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }];
+}
+
+interface ProductPageProp {
+  params: Promise<{ id: string }>;
+}
+
+const ProductPage = async ({ params }: ProductPageProp) => {
+  const { id } = await params;
+  return (
+    <h1>
+      Details of Product {id} redered at {new Date().toLocaleTimeString()}
+    </h1>
+  );
+};
+
+export default ProductPage;
---
+++ Tutorial 1/rendering-demo/src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const ProductsPage = () => {
+  return (
+    <>
+      <h1>Featured Products</h1>
+      <Link href="/products/1">Product 1</Link>
+      <Link href="/products/2">Product 2</Link>
+      <Link href="/products/3">Product 3</Link>
+    </>
+  );
+};
+
+export default ProductsPage;
