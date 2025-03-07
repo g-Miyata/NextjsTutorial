6ff4e3989c54f84029d475c015a097addf438012
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-07 15:15:21 -0300

    Third Party Packages

--- Tutorial 1/rendering-demo/src/app/server-route/page.tsx
+++ Tutorial 1/rendering-demo/src/app/server-route/page.tsx
@@ -1,7 +1,13 @@
+import ImageSlider from '@/components/ImageSlider';
 import serverSideFunction from '@/utils/server-utils';
 const ServerRoutePage = () => {
   const result = serverSideFunction();
-  return <h1>Server Route {result}</h1>;
+  return (
+    <>
+      <h1>Server Route {result}</h1>
+      <ImageSlider />;
+    </>
+  );
 };
 
 export default ServerRoutePage;
--- Tutorial 1/rendering-demo/src/components/ImageSlider.tsx
+++ Tutorial 1/rendering-demo/src/components/ImageSlider.tsx
@@ -4,17 +4,12 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-// import { useTheme } from "@/components/theme-provider";
 
-export default function ClientRoutePage() {
-  //   const theme = useTheme();
+export default function ImageSlider() {
   const settings = {
     dots: true,
   };
   return (
-    // <div style={{ color: theme.colors.primary }}>
-    //   <h1>Client route page</h1>
-    // </div>
     <div className="image-slider-container">
       <Slider {...settings}>
         <div>
