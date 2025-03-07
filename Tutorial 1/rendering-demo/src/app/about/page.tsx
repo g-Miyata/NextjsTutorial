6d940407f14d79ca572d21d141a3377034d78231
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-07 13:48:39 -0300

    Static Rendering

--- Tutorial 1/rendering-demo/src/app/about/page.tsx
+++ Tutorial 1/rendering-demo/src/app/about/page.tsx
@@ -1,6 +1,6 @@
 const AboutPage = () => {
   console.log('About server component');
-  return <h1>About Page</h1>;
+  return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 };
 
 export default AboutPage;
--- Tutorial 1/rendering-demo/src/app/page.tsx
+++ Tutorial 1/rendering-demo/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <Link href="/dashboard">Dashboard</Link>
+        <Link href="/about">About</Link>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">src/app/page.tsx</code>.
