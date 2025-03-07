2d129b95a4fdd66f7580370ee061ba46c045fcb4
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-07 14:00:45 -0300

    Dynamic Rendering

--- Tutorial 1/rendering-demo/src/app/about/page.tsx
+++ Tutorial 1/rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,8 @@
-const AboutPage = () => {
+import { cookies } from 'next/headers';
+const AboutPage = async () => {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get('theme');
+  console.log(theme);
   console.log('About server component');
   return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 };
