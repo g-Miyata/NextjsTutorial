944b6719162161cebfb6cf7e23e2f3c6dc204221
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-09 13:08:32 -0300

    Form Component

---
+++ Tutorial 1/data-fetching-demo/src/app/products-db/loading.tsx
@@ -0,0 +1,5 @@
+const LoadingProducts = () => {
+  return <div>Loading Products...</div>;
+};
+
+export default LoadingProducts;
--- Tutorial 1/data-fetching-demo/src/app/products-db/page.tsx
+++ Tutorial 1/data-fetching-demo/src/app/products-db/page.tsx
@@ -8,8 +8,13 @@ export type Product = {
   description: string | null;
 };
 
-const ProductsDBPage = async () => {
-  const products: Product[] = await getProducts();
+const ProductsDBPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const { query } = await searchParams;
+  const products: Product[] = await getProducts(query);
 
   return <ProductDetails products={products} />;
 };
